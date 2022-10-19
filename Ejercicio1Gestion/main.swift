2d53e2594a9b638096d73ede3c65c4c64bcd3312
Juan David Medina <juan_david_medina_m-tsapp1ma2122@cev.com>
2022-10-19 12:47:38 +0200

    Cuarto cambio

--- Ejercicio1Gestion/main.swift
+++ Ejercicio1Gestion/main.swift
@@ -7,11 +7,51 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
-// Este es la tarea 1 de la asignatura de gestión
+// Esta es la tarea 1 de la asignatura de gestión
 
-print("He realizado mi segundo cambio")
+//print("He realizado mi segundo cambio")
 
 // Subiré las tareas 1 y 2 de Ios para simbolizar el cuarto y el quinto cambio respectivamente.
 // Este es el tercero
+
+
+//TAREA 1
+
+var Contador = 0
+var numero: Float?
+var numero2: Float?
+var numero3: Float?
+var numero4: Float?
+var numero5: Float?
+var resultadosuma: Float
+var resultadomedia: Float
+
+print("Calculo de la media de 5 números que elija el usuario")
+    
+
+repeat {
+    print("Dime un número para hacer la media")
+        let numeroconsola = readLine()
+            Contador+=1
+    
+        if numero == nil {
+            numero = Float(numeroconsola!)
+        } else if numero2 == nil {
+            numero2 = Float(numeroconsola!)
+        } else if numero3 == nil {
+            numero3 = Float(numeroconsola!)
+        } else if numero4 == nil {
+            numero4 = Float(numeroconsola!)
+        } else if numero5 == nil {
+            numero5 = Float(numeroconsola!)
+        }
+} while Contador < 5
+
+
+resultadosuma = numero! + numero2! + numero3! + numero4! + numero5!
+
+resultadomedia = resultadosuma / 5
+
+print("La media resultante es:", resultadomedia)
