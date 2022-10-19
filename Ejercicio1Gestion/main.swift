99d4f3d111759cc6036b81c0d70afc9bc060205b
Juan David Medina <juan_david_medina_m-tsapp1ma2122@cev.com>
2022-10-19 12:45:46 +0200

    Tercer cambio

--- Ejercicio1Gestion/main.swift
+++ Ejercicio1Gestion/main.swift
@@ -12,3 +12,6 @@ print("Hello, World!")
 // Este es la tarea 1 de la asignatura de gestión
 
 print("He realizado mi segundo cambio")
+
+// Subiré las tareas 1 y 2 de Ios para simbolizar el cuarto y el quinto cambio respectivamente.
+// Este es el tercero
